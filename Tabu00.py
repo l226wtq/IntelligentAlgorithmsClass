7ee63cae3a37c63c7a86894d0159260610871cbc
Akria <857709855@qq.com>
2019-04-01 11:37:34 +0800

    提交一个网上找的，用于对照

--- Tabu00.py
+++ Tabu00.py
@@ -3,30 +3,33 @@
 '''
 @author: 85770
 @contact: 14290385@outlook.com
-@file: hi.py
+@file: Tabu.py
 @time: 2019/3/11 14:21
 '''
 import copy
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 print('hi')
-# city_x = [41, 37, 54, 25, 7, 2, 68, 71, 54, 83, 64, 18, 22, 83, 91, 25, 24, 58, 71, 74, 87, 18, 13, 82, 62, 58, 45, 41,
-#           44, 4]
-# city_y = [94, 84, 67, 62, 64, 99, 58, 44, 62, 69, 60, 54, 60, 46, 38, 38, 42, 69, 71, 78, 76, 40, 40, 7, 32, 35, 21, 26,
-#           35, 50]
+startTime = time.time()
+city_x = [41, 37, 54, 25, 7, 2, 68, 71, 54, 83, 64, 18, 22, 83, 91, 25, 24, 58, 71, 74, 87, 18, 13, 82, 62, 58, 45, 41,
+          44, 4]
+city_y = [94, 84, 67, 62, 64, 99, 58, 44, 62, 69, 60, 54, 60, 46, 38, 38, 42, 69, 71, 78, 76, 40, 40, 7, 32, 35, 21, 26,
+          35, 50]
 # 坐标随机生成
 # city_randomX = city_x
 # city_randomY = city_y
 city_randomX = []
 city_randomY = []
-cityNum = int(input('enter the num of city:'))
-swapNum = 5
-betterSoFar = 100
+# cityNum = int(input('enter the num of city:'))
+cityNum = 30
+swapNum = 14
+betterSoFar = 80
 Current_tabu = 0
-Tabu_linitStep = 5
-Tabu_limitLength = 2000
+Tabu_linitStep = 50
+Tabu_limitLength = 500
 
 
 class city:
@@ -75,13 +78,32 @@ class solution():
                 if i == cityNum - 1:
                     plt.plot([x, x0], [y, y0], c='r')  # 忽略编译器错误
         plt.title("distance:%s" % (self.calDistance(self.solveList)))
-        plt.pause(0.3)
+        plt.pause(0.001)
+
+    def drawPlotOutput(self):
+        plt.clf()
+        for i in range(cityNum):
+            x = eval('city{}.x_localtion'.format(self.solveList[i]))
+            y = eval('city{}.y_localtion'.format(self.solveList[i]))
+            plt.scatter(x, y, c='b')
+            if i == 0:
+                x0 = copy.deepcopy(x)
+                y0 = copy.deepcopy(y)
+            else:
+                x2 = eval('city{}.x_localtion'.format(self.solveList[i - 1]))
+                y2 = eval('city{}.y_localtion'.format(self.solveList[i - 1]))
+                plt.plot([x, x2], [y, y2], c='r')
+                if i == cityNum - 1:
+                    plt.plot([x, x0], [y, y0], c='r')  # 忽略编译器错误
+        plt.title("distance:%s" % (self.calDistance(self.solveList)))
+        plt.show()
+
 
 # 生成随机坐标
-for i in range(cityNum):
-    city_randomX.append(random.randint(0, 200))
-    city_randomY.append(random.randint(0, 200))
-for x, y, i in zip(city_randomX, city_randomY, range(cityNum)):
+# for i in range(cityNum):
+#     city_randomX.append(random.randint(0, 200))
+#     city_randomY.append(random.randint(0, 200))
+for x, y, i in zip(city_x, city_y, range(cityNum)):
     exec('city{}=city(x, y)'.format(i))
     exec('print(city{}.x_localtion , city{}.y_localtion)'.format(i, i))
 
@@ -123,7 +145,7 @@ for i in range(Tabu_limitLength):
         exI += 2
 
     swapMax = max(swapDisList)
-    if swapMax < -(betterSoFar / 3):
+    if swapMax < -(betterSoFar / 16):
         continue
     maxIndex = swapDisList.index(swapMax)
     # print(swapDisList.index(swapMax))
@@ -132,7 +154,8 @@ for i in range(Tabu_limitLength):
         Solution.distance = Solution.calDistance(Solution.solveList)
         Tabu_limitList[randomList[maxIndex * 2]][randomList[maxIndex * 2 + 1]] = Tabu_linitStep
         Tabu_limitList[randomList[maxIndex * 2 + 1]][randomList[maxIndex * 2]] = Tabu_linitStep  # 禁忌置数
-        Solution.drawPlot()
+        # Solution.drawPlot()
+
         print(Tabu_limitList)
     else:
         if swapMax > betterSoFar:
@@ -144,6 +167,9 @@ for i in range(Tabu_limitLength):
             print(Tabu_limitList)
         else:
             print('In Tabu,banned!')
+endTime = time.time()
+print('time', endTime - startTime)
+Solution.drawPlotOutput()
 # for i in range(10):
 #     np.random.shuffle(init_list)  # 随机排列
 #     print(init_list)
